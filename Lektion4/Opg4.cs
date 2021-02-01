cdcdec0b6bf8e40e292d386cd9d80fc4e112e162
Megl <mikkelskydstofte@protonmail.com>
2021-02-01 11:06:23 +0100

    starten på C#

---
+++ Lektion4/Opg4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lektion4
+{
+    class Opg4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lektion4_opg5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lektion4_opg5
+{
+    class Program
+    {
+        public static double Max(double[] arr)
+        {
+            double max = 0;
+            foreach (double num in arr)
+            {
+                if(num > max)
+                {
+                    max = num;
+                }
+            }
+
+            return max;
+         
+        }
+
+        static void Main(string[] args)
+        {   
+            double[] arr1 = new double[2]{ 28.5, 17.2};
+            double[] arr2 = new double[5] { 4.0, 10.8, 34.25, 2.0, 23.6 };
+
+            double m1 = Max(arr1);
+            double m2 = Max(arr2);
+
+            Console.WriteLine(m1 + " " + m2);
+            Console.ReadLine();
+
+        }
+    }
+}
